ec5760d4fb794f5c66c628eaaa9eb502a4488bb5
mhsatman <mhsatman@gmail.com>
2012-05-31 16:11:42 +0300

    readline integrated to REPL. Now fuzuli REPL is able to autocomplete the source code and keep local history.

--- Main/src/main.cpp
+++ Main/src/main.cpp
@@ -19,9 +19,9 @@
 #include <iostream>
 #include <stdlib.h>
 #include <cstring>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include "FuzuliTypes.h"
-
-
 
 using namespace std;
 using namespace fuzuli;
@@ -29,44 +29,56 @@ using namespace fuzuli;
 static int a_argc;
 static char **a_argv;
 
-void doRepl(){
+void doRepl() {
 	SourceCode *source = new SourceCode();
 	Environment *env = new Environment();
 	AstBuilder *ast;
 	Expression *expr;
 	Token *tok;
-	string line;
 
 	env->setFirst();
 	env->setArgcArgv(a_argc, a_argv);
-	char *buffer = (char*)malloc(1024);
-	while(1){
-		cout << "F: ";
-		cin.getline(buffer,1023);
-		line = string(buffer); line.append("\r\n");
-		source->readFromText(&line);
+
+	char* input;
+
+	while (1) {
+		rl_bind_key('\t', rl_complete);
+
+
+		input = readline("F: ");
+
+		if (!input) {
+			break;
+		}
+
+		add_history(input);
+		string code = string(input) + "\r\n";
+		source->readFromText(&code);
 		source->reset();
 		ast = new AstBuilder(source);
-		while( 1 ){
-				expr = ast->getNextExpression();
-				if(!expr) break;
-				tok = expr->eval(env);
-				cout << tok->getContent() << endl;
-				//Environment::GCEnvironment->GC();
+		expr = ast->getNextExpression();
+		if (!expr) {
+			break;
 		}
+		tok = expr->eval(env);
+		cout << tok->getContent() << endl;
+		//Environment::GCEnvironment->GC();
+
+		free(input);
 	}
+
 }
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
 	a_argc = argc;
 	a_argv = argv;
-	if(argc == 1){
+	if (argc == 1) {
 		cout << "Fuzuli build " << __DATE__ << " " << __TIME__ << endl;
 		cout << "usage:" << endl;
-		cout << " fuzuli source"<< endl;
-		cout << " fuzuli --repl"<< endl;
+		cout << " fuzuli source" << endl;
+		cout << " fuzuli --repl" << endl;
 		exit(0);
-	}else if(argc == 2 && strcmp(argv[1], "--repl")==0){
+	} else if (argc == 2 && strcmp(argv[1], "--repl") == 0) {
 		doRepl();
 		exit(0);
 	}
@@ -82,10 +94,10 @@ int main(int argc, char** argv){
 	globalEnvironment->setArgcArgv(argc, argv);
 	Expression *ex;
 
-
-	while( 1 ){
+	while (1) {
 		ex = b->getNextExpression();
-		if(!ex) break;
+		if (!ex)
+			break;
 		ex->eval(globalEnvironment);
 		//Environment::GCEnvironment->GC();
 	}
@@ -94,7 +106,4 @@ int main(int argc, char** argv){
 	//t->toString();
 
 }
-
-
-
 
