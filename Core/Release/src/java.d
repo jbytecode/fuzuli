881d301541ef52c923194dee1ffbf52290226739
mhsatman <mhsatman@gmail.com>
2012-06-08 20:12:36 +0300

    Some attempt to oop

--- Core/Release/src/java.d
+++
@@ -1,9 +0,0 @@
-src/java.d: ../src/java.cpp ../../Interpreter/include/FuzuliTypes.h \
- /usr/lib/jvm/default-java/include/jni.h \
- /usr/lib/jvm/default-java/include/jni_md.h
-
-../../Interpreter/include/FuzuliTypes.h:
-
-/usr/lib/jvm/default-java/include/jni.h:
-
-/usr/lib/jvm/default-java/include/jni_md.h:
