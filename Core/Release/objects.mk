0ade142478a1946f807338bd44c0a3e67ba27227
mhsatman <mhsatman@gmail.com>
2012-06-10 02:00:56 +0300

    Tcl Tk event handling implemented.

--- Core/Release/objects.mk
+++ Core/Release/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS :=
+USER_OBJS := /usr/lib/fuzuli/cpptk.so
 
 LIBS := -lm -lssl -lcrypto -lsocket -ltk -lgd -lmysqlclient -ltcl
 
--- Core/src/TclTk.cpp
+++ Core/src/TclTk.cpp
@@ -19,20 +19,60 @@
 
 #include "FuzuliTypes.h"
 #include <iostream>
+#include <string.h>
+#include <stdlib.h>
+#include <sstream>
+#include <vector>
 #include <tcl/tcl.h>
 #include <tk/tk.h>
+
 
 namespace fuzuli {
 
 extern "C" {
 Token *tcltk_init(Token* params, Environment *env);
 Token *tcltk_run(Token* params, Environment *env);
+Token *tcltk_add(Token* params, Environment *env);
 Token *tcltk_doevents(Token* params, Environment *env);
+Token *tcltk_mainloop(Token* params, Environment *env);
+Token *tcltk_create_command(Token *params, Environment *env);
 }
 
 using namespace std;
 
 static Tcl_Interp *interpreter;
+static stringstream ss;
+
+class CommandInfo {
+public:
+	Environment *env;
+	const char *command;
+};
+
+int DefaultCommand(ClientData clientdata, Tcl_Interp *interpreter,
+		int argc, char **argv) {
+	CommandInfo *ci = reinterpret_cast<CommandInfo*>(clientdata);
+	//cout << "Event Client: "<< ci->command << endl;
+	Environment *env = ci->env;
+
+	vector<Expression*> funcvec;
+	funcvec.push_back(new IdentifierExpression(new Token(ci->command,STRING)));
+	FunctionCallExpression *fce = new FunctionCallExpression(funcvec);
+	Token *result = fce->eval(env);
+    return 0;
+}
+
+Token *tcltk_create_command(Token *params, Environment *env){
+	const char *commandname = params->tokens[0]->getContent();
+	CommandInfo *ci = new CommandInfo();
+	ci->env = env;
+	ci->command = commandname;
+	ClientData cd = reinterpret_cast<ClientData>(ci);
+	Tcl_CreateCommand(interpreter, commandname, (Tcl_CmdProc*) DefaultCommand, cd, NULL);
+	cout << "Command created "<<commandname<<endl;
+	return(Token::NULL_TOKEN);
+}
+
 
 Token *tcltk_init(Token* params, Environment *env) {
 	Token *result = env->newToken(0.0, INTEGER);
@@ -48,12 +88,10 @@ Token *tcltk_init(Token* params, Environment *env) {
 	}
 	//registerEvents();
 	result->setIntValue(interpreter->errorLine);
-	return (result);
+	return(result);
 }
 
-void registerEvents() {
-	cout << "Registering events" << endl;
-}
+
 
 Token *tcltk_run(Token* params, Environment *env) {
 	int ti = Tcl_Eval(interpreter, params->tokens[0]->getContent());
@@ -61,12 +99,19 @@ Token *tcltk_run(Token* params, Environment *env) {
 	result->setIntValue(ti);
 	cout << "Returning " << ti << " for command "
 			<< params->tokens[0]->getContent() << endl;
+	ss.clear();
 	return (result);
 }
 
 Token *tcltk_doevents(Token* params, Environment *env) {
 	Tk_DoOneEvent(TCL_ALL_EVENTS);
 	return (Token::NULL_TOKEN);
+}
+
+
+Token *tcltk_mainloop(Token* params, Environment *env){
+	Tk_MainLoop();
+	return(Token::NULL_TOKEN);
 }
 
 }
