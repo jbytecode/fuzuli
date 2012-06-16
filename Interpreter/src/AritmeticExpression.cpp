70a9722711276247451b403dc022f7e6372707c7
mhsatman <mhsatman@gmail.com>
2012-06-16 16:43:34 +0300

    dec and inc modified.

--- Interpreter/src/AritmeticExpression.cpp
+++ Interpreter/src/AritmeticExpression.cpp
@@ -219,7 +219,7 @@ Token *IncExpression::eval(Environment *env) {
 	Token *name = ((IdentifierExpression*) this->expressions[0])->stringToken;
 	Environment *varenv = env->searchBackEnvironments(name->getContent());
 	Token *val = this->expressions[0]->eval(env);
-	Token *result = env->newToken (val->getFloatValue()+1, FLOAT);
+	Token *result = env->newToken (val->getFloatValue()+1.0, FLOAT);
 	val->ReduceReferences();
 	result->IncreaseReferences();
 	varenv->setVariable(name->getContent(), result);
@@ -245,7 +245,7 @@ Token *DecExpression::eval(Environment *env) {
 	Token *name = ((IdentifierExpression*) this->expressions[0])->stringToken;
 	Environment *varenv = env->searchBackEnvironments(name->getContent());
 	Token *val = this->expressions[0]->eval(env);
-	Token *result = env->newToken (val->getFloatValue()-1, FLOAT);
+	Token *result = env->newToken (val->getFloatValue()-1.0, FLOAT);
 	varenv->setVariable(name->getContent(), result);
 	return(result);
 }
