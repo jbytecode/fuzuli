92c1262018497a5ac9bc92e009d35b6c5ea79787
mhsatman <mhsatman@gmail.com>
2012-06-20 20:38:13 +0300

    Gui source seperated but in same dll because of include collusion in Windows built.

--- Core/Release/src/subdir.mk
+++ Core/Release/src/subdir.mk
@@ -7,7 +7,6 @@ CPP_SRCS += \
 ../src/IO.cpp \
 ../src/Math.cpp \
 ../src/MySql.cpp \
-../src/fltk.cpp \
 ../src/gd.cpp \
 ../src/ncurses.cpp \
 ../src/net.cpp \
@@ -18,7 +17,6 @@ OBJS += \
 ./src/IO.o \
 ./src/Math.o \
 ./src/MySql.o \
-./src/fltk.o \
 ./src/gd.o \
 ./src/ncurses.o \
 ./src/net.o \
@@ -29,7 +27,6 @@ CPP_DEPS += \
 ./src/IO.d \
 ./src/Math.d \
 ./src/MySql.d \
-./src/fltk.d \
 ./src/gd.d \
 ./src/ncurses.d \
 ./src/net.d \
@@ -41,7 +38,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -I/usr/include/tcl -I/usr/include/FL -I../../Interpreter/include/ -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I../../Interpreter/include/ -O3 -Wall -c -fmessage-length=0 -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
