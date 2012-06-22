fe29a82951791c4d0f5ae7b2f3cb066b9300a3a1
mhsatman <mhsatman@gmail.com>
2012-06-22 15:35:54 +0300

    more on fltk library

--- Core/src_gui/fltk.cpp
+++ Core/src_gui/fltk.cpp
@@ -98,7 +98,12 @@ Token *fl_pointd(Token *p, Environment *env);
 Token *fl_circled(Token *p, Environment *env);
 Token *fl_fontd(Token *p, Environment *env);
 Token *fl_textd(Token *p, Environment *env);
-
+Token *fl_textwidth(Token *p, Environment *env);
+Token *fl_rectd(Token *p, Environment *env);
+Token *fl_arcd(Token *p, Environment *env);
+Token *fl_arc2d(Token *p, Environment *env);
+Token *fl_pied(Token *p, Environment *env);
+Token *fl_rotated(Token *p, Environment *env);
 }
 
 void default_callback(Fl_Widget* widget, void* p) {
@@ -283,6 +288,61 @@ Token *fl_textd(Token *p, Environment *env) {
 	int x = p->tokens[0]->getIntValue();
 	int y = p->tokens[1]->getIntValue();
 	fl_draw(p->tokens[2]->getContent(), x, y);
+	return (Token::NULL_TOKEN);
+}
+
+Token *fl_textwidth(Token *p, Environment *env) {
+	double dres = fl_width(p->tokens[0]->getContent());
+	Token *result = env->newToken(dres, FLOAT);
+	return (result);
+}
+
+
+Token *fl_rectd(Token *p, Environment *env){
+	int x = p->tokens[0]->getIntValue();
+	int y = p->tokens[1]->getIntValue();
+	int w = p->tokens[2]->getIntValue();
+	int h = p->tokens[3]->getIntValue();
+	fl_rectf(x,y,w,h);
+	return(Token::NULL_TOKEN);
+}
+
+Token *fl_arcd(Token *p, Environment *env){
+	int x = p->tokens[0]->getIntValue();
+	int y = p->tokens[1]->getIntValue();
+	int w = p->tokens[2]->getIntValue();
+	int h = p->tokens[3]->getIntValue();
+	double a1 = p->tokens[4]->getIntValue();
+	double a2 = p->tokens[5]->getIntValue();
+	fl_arc(x,y,w,h,a1,a2);
+	return(Token::NULL_TOKEN);
+}
+
+
+Token *fl_arc2d(Token *p, Environment *env){
+	double x = p->tokens[0]->getFloatValue();
+	double y = p->tokens[1]->getFloatValue();
+	double r = p->tokens[2]->getFloatValue();
+	double start = p->tokens[3]->getFloatValue();
+	double end = p->tokens[4]->getFloatValue();
+	fl_arc(x,y,r,start,end);
+	return(Token::NULL_TOKEN);
+}
+
+Token *fl_pied(Token *p, Environment *env){
+	int x = p->tokens[0]->getIntValue();
+	int y = p->tokens[1]->getIntValue();
+	int w = p->tokens[2]->getIntValue();
+	int h = p->tokens[3]->getIntValue();
+	double a1 = p->tokens[4]->getIntValue();
+	double a2 = p->tokens[5]->getIntValue();
+	fl_pie(x,y,w,h,a1,a2);
+	return(Token::NULL_TOKEN);
+}
+
+Token *fl_rotated(Token *p, Environment *env) {
+	double r = p->tokens[0]->getFloatValue();
+	fl_rotate(r);
 	return (Token::NULL_TOKEN);
 }
 
