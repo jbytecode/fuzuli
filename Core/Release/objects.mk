a14237670cf675aade368f5e94d9b2d0fef02c58
mhsatman <mhsatman@gmail.com>
2012-09-13 11:59:38 +0300

    Project configuration files and test.php updated.

--- Core/Release/objects.mk
+++ Core/Release/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := 
+USER_OBJS := /home/hako/workspace/fuzuli/Interpreter/Release/libfuzuli.so
 
 LIBS := -lm -lssl -lcrypto
 
--- tests/tester.php
+++ tests/tester.php
@@ -19,73 +19,73 @@
 
 $fuzuli_bin="/usr/lib/fuzuli/fuzuli";
 
-$fuzuli_temp=tempnam("/temp", "fuzuli_result");
+$fuzuli_temp=tempnam ("/temp", "fuzuli_result");
 
-$total_passed=0; 
-$total_error=0; 
+
+$total_passed=0;
+$total_error=0;
 $files_count=0;
-$total_unknown=0; 
+$total_unknown=0;
 
-print "--------------------------------------------\n";
-print "|            Fuzuli Test Result            |\n";
-print "--------------------------------------------\n";
+print ("--------------------------------------------\n");
+print ("|            Fuzuli Test Result            |\n");
+print ("--------------------------------------------\n");
 
 #list for .fzl files
 $path=(".");
-$folder=opendir($path);
-	if($folder) {
-		while($file=readdir($folder))   {
-			if(!is_dir($file)) {
-				if(substr($file, -3)=="fzl") {
-					$files[]=$file;
-					$files_count++;
-				}
-			}
-		}
-	}
-sort($files);
+$folder=opendir ($path);
+   if($folder) {
+      while ($file=readdir ($folder))   {
+         if (!is_dir ($file)) {
+            if (substr ($file, -3)=="fzl") {
+               $files[]=$file;
+               $files_count++;
+            }
+         }
+      }
+   }
+sort ($files);
 
-	foreach($files as $eachfile) {
-		if(substr($eachfile, -3)=="fzl") {
-			$newfile=$eachfile;
-			if($newfile!=NULL) {
-				$last_process=system(''.$fuzuli_bin.' '.$newfile.' > '.$fuzuli_temp.'', $process_result);
-				print trim($newfile).":\n";
-					if(!$process_result) { 
-						print "";
-					} else {
-						print "Error Code: ".$process_result."\n";				
-						$total_unknown=$total_unknown+1;
-						$error_files[]=$newfile;
-					}
-        $file_content = file_get_contents($fuzuli_temp);
-				print $file_content;
-				print "--\n";
-				$tests=explode("\n", $file_content);
-				foreach($tests as $test) {
-					if($test=="PASSED!") {
-						$total_passed=$total_passed+1;	
-					}
-					elseif($test=="ERROR") {
-						$total_error=$total_error+1;	
-					}
-				}
-			}
-		}
-	}
+   foreach ($files as $eachfile) {
+      if (substr( $eachfile, -3)=="fzl") {
+         $newfile=$eachfile;
+         if ($newfile!=NULL) {
+            $last_process=system(''.$fuzuli_bin.' '.$newfile.' > '.$fuzuli_temp.'', $process_result);
+            print (trim ($newfile).":\n");
+               if (!$process_result) {
+                  print ("");
+               } else {
+                  print ("Error Code: ".$process_result."\n");            
+                  $total_unknown=$total_unknown+1;
+                  $error_files[]=$newfile;
+               }
+            $file_content=file_get_contents ($fuzuli_temp);
+            print ($file_content);
+            print ("--\n");
+            $tests=explode ("\n", $file_content);
+            foreach ($tests as $test) {
+               if ($test=="PASSED!") {
+                  $total_passed=$total_passed+1;   
+               }
+               elseif ($test=="ERROR") {
+                  $total_error=$total_error+1;   
+               }
+            }
+         }
+      }
+   }
 
-print "--------------------------------------------\n";
-print "|                Test Reports              |\n";
-print "--------------------------------------------\n";
-print "|  ".$files_count." files have been tested\n";
-print "|  ".$total_passed." statements have been passed.\n";
-print "|  ".$total_error." statements have been failed.\n";
-print "|  ".$total_unknown." files have been undefined:.\n";
-	if($total_unknown>"0") {
-		foreach($error_files as $error_file) {
-			print "|    -".$error_file." file\n";	
-		}
-	}
-print "--------------------------------------------\n";
+print ("--------------------------------------------\n");
+print ("|                Test Reports              |\n");
+print ("--------------------------------------------\n");
+print ("|  ".$files_count." files have been tested\n");
+print ("|  ".$total_passed." statements have been passed.\n");
+print ("|  ".$total_error." statements have been failed.\n");
+print ("|  ".$total_unknown." files have been undefined:.\n");
+   if ($total_unknown>"0") {
+      foreach ($error_files as $error_file) {
+         print ("|    -".$error_file." file\n");   
+      }
+   }
+print ("--------------------------------------------\n");
 ?>
-
