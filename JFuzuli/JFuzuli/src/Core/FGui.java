d6d41c78efd8769e5a5b1f8a6579c125f931bd03
mhsatman <mhsatman@gmail.com>
2013-02-10 22:34:29 +0200

    Started to FGui internals for JFuzuli

---
+++ JFuzuli/JFuzuli/src/Core/FGui.java
@@ -0,0 +1,28 @@
+/*
+ * fuzuli : A general purpose interpreter
+ * Copyright (C) 2013 Mehmet Hakan Satman <mhsatman@yahoo.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package Core;
+
+import Interpreter.Environment;
+
+public class FGui {
+
+    public static void msgbox (String text, Environment env){
+        javax.swing.JOptionPane.showMessageDialog(null, text);
+    }
+}
