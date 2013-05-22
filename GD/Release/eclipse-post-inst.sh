35891f181235e6677bcf0dc4985685231b3817f4
mhsatman <mhsatman@gmail.com>
2013-05-22 15:54:54 +0300

    C1.0 - GD and Thread packages removed

--- GD/Release/eclipse-post-inst.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo cp libFuzuliGd.so /usr/lib/fuzuli/libFuzuliGd.so
-sudo cp -R ../nfl/ /usr/lib/fuzuli/
-sudo ldconfig /usr/lib/fuzuli
-
--- Thread/Release/eclipse-post-inst.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo cp libFuzuliThread.so /usr/lib/fuzuli/libFuzuliThread.so
-sudo cp -R ../nfl/ /usr/lib/fuzuli/
-sudo ldconfig /usr/lib/fuzuli
-
