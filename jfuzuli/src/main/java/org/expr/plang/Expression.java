fab8f3dda0fa9a588f84704a805c5d5c569b0e02
hako <mhsatman@gmail.com>
2020-01-21 12:33:24 +0300

    package name corrected

---
+++ jfuzuli/src/main/java/org/expr/plang/Expression.java
@@ -0,0 +1,11 @@
+package org.expr.plang;
+
+public abstract class Expression {
+
+	public abstract Object eval(Environment env);
+        
+        public String emitJS(){
+            return "JS Emitter is not implemented for " + this.toString();
+        }
+	
+}
